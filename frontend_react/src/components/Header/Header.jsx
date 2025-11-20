ecc306dc54508eabcb9ec7507435d427236a7767
Avdo Hrnjić <avdohrnjic@hotmail.com>
2025-11-20 14:08:31 +0100

    Rad na headeru

--- frontend_react/src/components/Header/Header.jsx
+++ frontend_react/src/components/Header/Header.jsx
@@ -1,17 +1,44 @@
 import React,{useState, useRef} from 'react'
-import Zeljo from "../../assets/svg/zeljo.svg"
+import Zeljo from "../../assets/svg/zeljo_white_icon.svg"
+import ArrowDown from "../../assets/svg/arrow_down.svg"
+import User from "../../assets/svg/user.svg"
 import "./header.css"
 
 const Header = () => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
-    <header id="header-container" className="collapsed" style={{height:isExpanded ? "500px" : "100px"}}> 
+    <header id="header-container" style={{height:isExpanded ? "500px" : "100px"}}> 
         <div id="logo-option">
             <img src={Zeljo} alt="logo"></img>
         </div>
-        <div id="news-option">
-          Novosti
+        <div className="header-options">
+          <div id="news-option" className="header-option">
+            Novosti
+            <img src={ArrowDown} alt="ˇ" className='arrow-down'></img>
+          </div>
+          <div id="shop-option" className="header-option">
+            Shop
+            <img src={ArrowDown} alt="ˇ" className='arrow-down'></img>
+          </div>
+          <div id="tickets-option" className="header-option">
+            Ulaznice
+            <img src={ArrowDown} alt="ˇ" className='arrow-down'></img>
+          </div>
+          <div id="member-option" className="header-option">
+            Članstvo
+            <img src={ArrowDown} alt="ˇ" className='arrow-down'></img>
+          </div>
+          <div id="about-option" className="header-option">
+            O nama
+            <img src={ArrowDown} alt="ˇ" className='arrow-down'></img>
+          </div>
+        </div>
+        <div id="user-option">
+          <img src={User} alt="" className='user-icon'></img>
+        </div>
+        <div id="sign-in-text">
+          Prijavi se
         </div>
     </header>
   )
