5afacaa2599c811b38d34e32c205b53c99529328
Avdo Hrnjić <avdohrnjic@hotmail.com>
2025-11-21 11:39:13 +0100

    Dodani paketi

--- backend/backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a651afec673c07e682714cf16e8cd45d223149dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28d4fb02314ae70c970d57e0d2ff95216559fa08")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
