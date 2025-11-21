a651afec673c07e682714cf16e8cd45d223149dd
Avdo Hrnjić <avdohrnjic@hotmail.com>
2025-11-21 11:26:46 +0100

    Provjera

--- backend/backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecc306dc54508eabcb9ec7507435d427236a7767")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a198359a4b392a7b9f7a36b9ca9345de7729e55")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
