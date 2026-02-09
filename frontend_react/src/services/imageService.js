2fad325f9f7a82fc5547c8017253b211e92795bc
Avdo Hrnjić <avdohrnjic@hotmail.com>
2026-02-09 14:34:08 +0100

    Update imageService.js

--- frontend_react/src/services/imageService.js
+++ frontend_react/src/services/imageService.js
@@ -1,7 +1,5 @@
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
-console.log(import.meta.env.VITE_BACKEND_URL);
-
 export const getImageUrl = (path) => {
     return `${BACKEND_URL}${path}`;
 }
