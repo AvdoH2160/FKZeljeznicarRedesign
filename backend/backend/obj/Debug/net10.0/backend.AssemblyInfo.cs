c1b972a694d2fde3b93d454d4ab4907758b6c8fd
Avdo Hrnjić <avdohrnjic@hotmail.com>
2026-03-08 17:18:43 +0100

    Picture volume fix

--- backend/backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+709b9ce1a4e0bef2edf7b9ac6df3eb43ff98f30d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a98bc720a26409e915d03c038681cfe354c3873d")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
